03a7af3cc7ae01301903fc52910634afb9ca7179
zaidamorales <60615803+zaidamorales@users.noreply.github.com>
2022-01-18 20:21:16 +0100

    Update main.c

--- ejercicio 3/main.c
+++ ejercicio 3/main.c
@@ -14,18 +14,3 @@ void main(){
     n=validate(n);
     printf("En binario es: %d",binario(n));
 }
-
-/*
-void main(){
-    int count=1,rem,n,bin=0;
-    printf("Introduce a natural number:\n");
-    scanf("%d",&n);
-
-    while(n!=0){
-                    rem = n % 2;
-                    n /= 2;
-                    bin = bin+rem*count ;
-                    count*=10;
-       }
-   printf("binary is:%d",bin);
-}*/
