2502bb98ade5d6092f2a5fad06b78a70a32c1b90
zaidamorales <60615803+zaidamorales@users.noreply.github.com>
2022-01-19 12:43:13 +0100

    Update functions.c

--- ejercicio1/functions.c
+++ ejercicio1/functions.c
@@ -18,17 +18,5 @@ int natural(int a, int b){
     	}
     }while (num<a || num>b && i !=3);
 
-
-/*
-    do{
-    	printf("Introdueix un nombre entre 10 i 5000 \n");
-    	scanf("%d", &num);
-    	i+=1;
-    			if (i==3){
-    				printf("Demasiados intentos \n");
-    				num=50;
-    			}
-    }while (num<a || num>b && i==3);
-*/
     return num;
 }
