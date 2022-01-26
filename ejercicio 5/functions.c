8d4bcc7017c5fa3a0d43a27ed4d29f9353a6fdb8
zaidamorales <60615803+zaidamorales@users.noreply.github.com>
2022-01-26 09:16:57 +0100

    Add files via upload

---
+++ ejercicio 5/functions.c
@@ -0,0 +1,18 @@
+/*
+ * functions.c
+ *
+ *  Created on: Jan 12, 2022
+ *      Author: admin-pg
+ */
+
+intercanvi(int num1, int num2){
+	int aux;
+
+	aux = num1;
+	num1 = num2;
+	num2 = aux;
+
+	printf ("Primer valor: %d \nSegundo valor: %d", num1, num2 );
+
+
+}
---
+++ ejercicio 5/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 12, 2022
+ *      Author: admin-pg
+ */
+
+
+#include <stdio.h>
+#include "functions.h"
+
+void main (){
+	int num1=0, num2=0;
+
+	printf("Introduce el primer valor:");
+	scanf("%d", &num1);
+	printf("Introduce el segundo valor:");
+	scanf("%d", &num2);
+
+	intercanvi(num1, num2);
+}
